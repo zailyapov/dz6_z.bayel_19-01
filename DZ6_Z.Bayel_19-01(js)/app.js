47ab8f9dcc8a54072380470367478fe5b0f1350d
zailyapov <115218244+zailyapov@users.noreply.github.com>
2022-10-22 00:56:39 +0600

    Add files via upload

---
+++ DZ6_Z.Bayel_19-01(js)/app.js
@@ -0,0 +1,26 @@
+// Задание 1
+let [elem1, elem2, elem3] = ['Xiaomi', 'Apple', 'HONOR'];
+let [arr] = ['HUAWEI', 'OPPO', 'Vivo']
+console.log(elem1)
+console.log(elem2)
+console.log(elem3)
+console.log(arr.join())
+
+
+// Задание 2
+let element = [elem1, elem2, elem3] = ['one', 'two', 'three'];
+element.reverse();
+console.log(element);
+
+// Задание 3
+function user(a= 'аноним') {
+    alert('привет,'+ a);
+}
+let name = 'Байэл';
+user ();
+alert(name);
+
+
+
+
+
